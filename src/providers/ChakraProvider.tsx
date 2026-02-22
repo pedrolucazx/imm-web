fe1eac66ce493ef756ce6eda747082210a97102e
Pedro Mesquita <pedrolucazxmesquita@gmail.com>
2026-02-22 19:53:10 -0300

    feat(providers): configure TanStack Query provider
    - Create ReactQueryProvider with QueryClient configuration
    - Set default staleTime to 1 minute
    - Disable refetch on window focus
    - Add React Query DevTools for development
    - Integrate ReactQueryProvider into app providers

--- src/providers/ChakraProvider.tsx
+++ src/providers/ChakraProvider.tsx
@@ -1,7 +1,12 @@
 "use client";
 
 import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
+import { ReactQueryProvider } from "./ReactQueryProvider";
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <ChakraProvider value={defaultSystem}>{children}</ChakraProvider>;
+  return (
+    <ReactQueryProvider>
+      <ChakraProvider value={defaultSystem}>{children}</ChakraProvider>
+    </ReactQueryProvider>
+  );
 }
