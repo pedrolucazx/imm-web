be9a476b6bac327dabb22ed4c415236c7e70e4f9
Pedro Mesquita <pedrolucazxmesquita@gmail.com>
2026-02-22 22:26:03 -0300

    chore: fix webpack cache warning and migrate lint script to eslint cli

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,13 @@
+import type { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
 const withNextIntl = createNextIntlPlugin("./i18n/request.ts");
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig: NextConfig = {
+  webpack(config) {
+    config.infrastructureLogging = { level: "error" };
+    return config;
+  },
+};
 
 export default withNextIntl(nextConfig);
